542ade9aa55a6348eb117fdc8943fecd6f2cf6a6
24ndoumbe <98036183+24ndoumbe@users.noreply.github.com>
2022-09-14 21:09:28 +0200

    Add files via upload

---
+++ boutique sport.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <link rel="stylesheet" href="style boutique sport.css">
+    <title>BOUTIQUE</title>
+</head>
+<body>
+<nav>
+        <a href="#" class="logo"><span>A</span>ll4<img src="logo sport.jfif" alt=""><span>S</span>port </a>
+        <h1></h1>
+        <div class="onglets">
+            <a href="#acceuil">Acceuil</a>
+            <a href="#presentation">Presentation</a>
+            <a href="#produits">Produits</a>
+            <a href="#services">Services</a>
+            <a href="#contact">Contact</a>
+        </div>
+
+    </nav>
+
+    <section class="main" id="produits">
+             
+            <section class="card" id="produits">
+                <div class="titre">
+                    <h1>PRODUITS</h1>
+                </div>
+                <div class="row">
+                    <div class="col-6 col-md-4">
+                        <div class="image">
+                            <img src="ensemble sport.jfif" alt="">
+                        </div>
+                        <div class="content">
+                            <a href="#" class="title">ensemble</a>
+                            <p>50£<br> boutique de LILLE</p>
+                        </div>
+                    </div>
+                    <div class="col-6 col-md-4">
+                            <div class="image">
+                                <img src="bouteille.jfif" alt="">
+                            </div>
+                            <div class="content">
+                                <a href="#" class="title">bouteille</a>
+                                <p>50£<br> boutique de LILLE</p>
+
+                            </div>
+                        </div>
+                    
+                    <div class="col-6 col-md-4">
+                                <div class="image">
+                                    <img src="chaussure de sport.jfif" alt="">
+                                </div>
+                                <div class="content">
+                                    <a href="#" class="title">chaussure</a>
+                                    <p>50£<br> boutique de LILLE</p>
+
+                                </div>
+                           </div>
+                           </div>
+
+
+
+    <footer>
+    <p id="contact">&copy;Contact : 00 00 00 00 00</p>
+    <div class="row">
+        <i class="fas fa-envelope"></i>
+    </div>
+</footer>
+</body>
+</html>
